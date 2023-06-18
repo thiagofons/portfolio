997a69330103ec980da38f83a8ab3eb6eb1247b2
thiago <thiago_arruda@id.uff.br>
2023-06-18 10:57:27 -0300

    Setup

---
+++ src/Projects.ts
@@ -0,0 +1,11 @@
+import "./img/projects/mynotes.png"
+
+export const projects = [
+  {
+    name: "MyNotes",
+    img: "./img/projects/mynotes.png",
+    text: "Create your own sticky notes on the browser!",
+    githubLink: "https://github.com/thiagofons/mynotes",
+    liveSiteLink: "https://thiagofons.github.io/mynotes/"
+  },
+]
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,5 +1,30 @@
+import "../styles/main.sass";
+
+import { useEffect, useState } from "react";
+import Project from "../components/layout/Project";
+import { projects } from "../Projects";
+
 const Projects = () => {
-  return <></>
-}
+  return (
+    <section id="projects" className="projects section">
+      <header className="projects__header">
+        <h2>Projects</h2>
+        <p>Here are some projects I developed to improve my front-end skills</p>
+      </header>
+      <main className="projects__carousel">
+        {projects &&
+          projects.map((project) => (
+            <Project
+              name={project.name}
+              img={project.img}
+              text={project.text}
+              githubLink={project.githubLink}
+              liveSiteLink={project.liveSiteLink}
+            />
+          ))}
+      </main>
+    </section>
+  );
+};
 
 export default Projects;
